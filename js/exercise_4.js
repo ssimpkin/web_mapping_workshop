7ee71ff4fae015acefdc2e999d5528604c98b001
Sarah Simpkin <ssimpkin@users.noreply.github.com>
2016-05-02 15:55:45 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -59,7 +59,6 @@ var clickHandler = function(){
       	info += '<h2>' + feature.properties.name + '</h2>';
       	if(feature.properites.cuisine) info += '<p>' + feature.properties.cuisine + '</p>';
       	if(feature.properites.phone) info += '<p>' + feature.properties.phone + '</p>';
-    	if(feature.properites.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a></p>';
       	info += '</div>'
         $('#info').append(info);
     })
