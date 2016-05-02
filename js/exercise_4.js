bd18d794d8245a99666120b1c8655971ff6f3d3d
Sarah Simpkin <ssimpkin@users.noreply.github.com>
2016-05-02 15:02:17 -0400

    Adds data

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -16,3 +16,25 @@ var map = L.mapbox.map('map', mapId);
 map.setView([39, -96], 4);
 
 // Great, now we have a basic web map!
+
+
+// Reference to the data we want to show
+var dataFileToAdd = 'data/restaurants.geojson';
+
+// Create a feature layer - construction function that creates it for us
+var featureLayer = L.mapbox.featureLayer();
+	featureLayer.loadURL(dataFileToAdd); // calls on file
+	featureLayer.addTo(map); // adds file as a layer
+
+featureLayer.on('ready', function(){ // need to run through each feature and note their style
+	this.eachLayer(function(layer){
+      layer.setIcon(L.mapbox.marker.icon({}
+      	"marker-color": "#8834bb",
+        "marker-size": "large",
+       	"marker-symbol": "restaurant"
+       }))
+      
+    })
+    map.fitBounds(featureLayer.getBounds());
+})
+
